09007309ab917d29e33d6ebd44c7d1f2f458d8dd
EduardOshurkov <drd2993@gmail.com>
2022-05-29 22:48:43 +0300

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
 
+
+const refs = {
+    form: document.querySelector(".feedback-form"), 
+    textarea: document.querySelector(".feedback-form textarea")
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+populateTextarea();
+
+
+refs.form.addEventListener('input', event => {
+    formData[event.target.name] = event.target.value;
+    console.log(formData);
+})
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+
+function onTextareaInput(event) {
+    const message = event.target.value;
+    
+    localStorage.setItem(STORAGE_KEY, message);
+}
+ 
+
+function populateTextarea () {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if (savedMessage) {
+        console.log(savedMessage);
+        refs.textarea.value = savedMessage;
+}
+ }
