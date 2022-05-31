12fd07705807a1ff3bcc1917532996e1b9e15abc
EduardOshurkov <drd2993@gmail.com>
2022-05-31 22:15:01 +0300

    saved function

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,28 +8,34 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.input.addEventListener('input', throttle(onInputValue, 500));
-refs.textarea.addEventListener('input', throttle(onTextareaValue, 500));
+refs.form.addEventListener('input', throttle(onInputValue, 500));
+refs.input.addEventListener('input', onInputValue);
+refs.textarea.addEventListener('input', onTextareaValue);
 
 const STORAGE_KEY = 'feedback-form-state';
 let formData = {};
 
-
-// refs.form.addEventListener('input', event => {
-//     formData[event.target.name] = event.target.value;
-// })
-
 function onFormSubmit(event) {
     event.preventDefault();
     event.currentTarget.reset();
-
     localStorage.removeItem(STORAGE_KEY);
+
+    if (!formData['email'] || !formData['message']) {
+       return  alert('Заполните все поля!')
+    } else {
+        console.log(formData);
+    }
 }
 
+// function clearData(object) {
+//     for (const key in object) {
+//         delete object[key];
+// }
+// }
 
 function onInputValue(event) {
     formData[event.target.name] = event.target.value;
-    const inputValueForStorage = JSON.stringify(formData)
+    const inputValueForStorage = JSON.stringify(formData);
 
     localStorage.setItem(STORAGE_KEY, inputValueForStorage);
 }
@@ -39,7 +45,19 @@ function onTextareaValue(event) {
     const textareaValueForStorage = JSON.stringify(formData)
     localStorage.setItem(STORAGE_KEY, textareaValueForStorage);
 }
- 
 
 
+function savedForm() {
+    formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (formData) {
+        returnSavedLocalStorage(formData);
+    } else formData = {};
+}
 
+savedForm();
+
+function returnSavedLocalStorage(formData) {
+    for (const key in formData) {
+        refs.form[key].value = formData[key];
+    }
+}
