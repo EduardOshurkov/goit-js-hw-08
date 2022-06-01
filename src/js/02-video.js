1501031b30e4b24bde8bdd7ec04abaef86ed7ca3
EduardOshurkov <drd2993@gmail.com>
2022-06-01 20:49:02 +0300

    did the task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+const timeOnStart = localStorage.getItem("videoplayer-current-time");
+if (timeOnStart) {
+    player.setCurrentTime(timeOnStart)
+}
+
+player.on('timeupdate', throttle(updateTime, 1000));
+
+function updateTime(data) {
+    localStorage.setItem("videoplayer-current-time", Number.parseInt(data.seconds))
+}
